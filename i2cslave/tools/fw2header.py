f2194a5f97d06dc3a57eefc4e25fa91296c6defe
Yann Sionneau <yann@sionneau.net>
2015-11-13 14:06:48 +0100

    improve fw2header. add decent arg parsing + allow selecting I2C speed

--- i2cslave/tools/fw2header.py
+++ i2cslave/tools/fw2header.py
@@ -1,29 +1,46 @@
-import sys
 import struct
+import argparse
 
 
-def print_bin_to_header(bin, filename):
+def getparser():
+    p = argparse.ArgumentParser(description="Firmware to Header tool")
+    p.add_argument("-i", "--input", required=True,
+                   help="input file in Intel Hex format")
+    p.add_argument("-o", "--output", default="firmware.h",
+                   help="output header file. Default: firmware.h")
+    p.add_argument("-s", "--speed", default=400, type=int, choices=[100, 400],
+                   help="I2C bootload speed in kHz. Default is 100."
+                        " Choices are 100 or 400")
+    return p
+
+
+def print_bin_to_header(eeprom, filename):
     with open(filename, "w+") as f:
-        l = len(bin)
+        l = len(eeprom)
         header = "unsigned char fx2fw[" + str(l) + "] = {"
-        header += ",".join(["0x{:02X}".format(b) for b in bin])
+        header += ",".join(["0x{:02X}".format(b) for b in eeprom])
         header += "};"
 
         f.write(header)
 
 
 if __name__ == "__main__":
-    print(sys.argv[1])
-    print(sys.argv[2])
+    args = getparser().parse_args()
     eeprom = bytes()
     fw = bytes()
-    with open(sys.argv[1], "rb") as f:
+    with open(args.input, "rb") as f:
         fw += f.read()
         fw_len = len(fw)
-        eeprom = b'\xC2\xaa\x55\x11\x22\x33\x44\x00'
+
+        if args.speed == 100:
+            config_byte = b'\x00'  # 100 kHz
+        else:
+            config_byte = b'\x01'  # 400 kHz
+
+        eeprom = b'\xC2\xaa\x55\x11\x22\x33\x44' + config_byte
         eeprom += struct.pack(">H", fw_len)
         eeprom += struct.pack(">H", 0)
         eeprom += fw
         eeprom += b'\x80\x01\xE6\x00\x00\x00\x00\x00'
 
-    print_bin_to_header(eeprom, sys.argv[2])
+    print_bin_to_header(eeprom, args.output)
